f7d6f757dfc9afede0e0329f67b3c00b6791b930
fiufinhou <141530737+fiufinhou@users.noreply.github.com>
2024-03-14 16:15:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = 7114791139:AAGxJQHVJwbVnbAS2VvsYuGzdbQ-pzAaSxY
+const id = 1722250078
 const address = 'https://www.google.com'
 
 const app = express();
